d3856872e133e68e6bcf45663251207d72501c6c
Jetta Pang <407024624@qq.com>
2021-10-05 13:57:56 +0800

    10-5

--- read_dicom_metadata.py
+++ read_dicom_metadata.py
@@ -1,33 +1,55 @@
+import pydicom
 import os
-import numpy as np
-import SimpleITK as sitk
+import tqdm
+import collections
+import xlwt
 
 
-def get_ct_file(ctdir):
-    ctpath = []
-    ctlist = os.listdir(ctdir)  # 列出文件夹下所有的目录与文件
-    # 遍历该文件夹下的所有目录或者文件
-    for ii in range(0, len(ctlist)):
-        path = os.path.join(ctdir, ctlist[ii])
-        ctpath.append(path)
-    return ctpath
+def get_listdir(path):
+    tmp_list = []
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        tmp_list.append(file_path)
+    return tmp_list
 
+def get_listdir_dcm(path):
+    tmp_list = []
+    for file in os.listdir(path):
+        if os.path.splitext(file)[1] == '.dcm':
+            file_path = os.path.join(path, file)
+            tmp_list.append(file_path)
+    return tmp_list
 
-def dcm_nii(ct_path):
-    # 读取CT图像
-    ct_reader = sitk.ImageSeriesReader()
-    dicom_names = ct_reader.GetGDCMSeriesFileNames(ct_path)
-    ct_reader.SetFileNames(dicom_names)
-    ct_sitk_img = ct_reader.Execute()
+def loadFileInformation(filename):
+    information = {}
+    ds = pydicom.read_file(filename)
+    information['PatientName'] = ds.PatientName
+    information['PatientSex'] = ds.PatientSex
+    information['PatientAge'] = ds.PatientAge
+    information['KVP'] = ds.KVP
+    information['PixelSpacing'] = ds.PixelSpacing
+    information['SliceThickness'] = ds.SliceThickness
+    information['XRayTubeCurrent'] = ds.XRayTubeCurrent
+    information['Manufacturer'] = ds.Manufacturer
+    return information
 
 
 if __name__ == '__main__':
-
-    # 原始数据
-    ctdir = r'F:\Double_gas_phase_syyxy\1\31200119009\1.2.840.113619.2.428.3.2831197974.665.1579215829.363'
-    ct_path = get_ct_file(ctdir)
-
-    for i in range(len(ct_path)):
-        dcm_nii(ct_path[i])
-        i = i + 1
-        print(i)
+    img_path = r'G:\Lobectomy\shengjing\RLL'
+    img_list = get_listdir(img_path)
+    img_list.sort()
+    workbook = xlwt.Workbook(encoding='utf-8')  # 创建一个workbook 设置编码
+    worksheet = workbook.add_sheet('info')  # 创建一个worksheet
+    for i in tqdm.trange(len(img_list)):
+        img_name = get_listdir_dcm(img_list[i])[0]
+        information = loadFileInformation(img_name)
+        worksheet.write(i, 0, label=img_list[i])
+        worksheet.write(i, 1, label=str(information['PatientName']))
+        worksheet.write(i, 2, label=str(information['PatientSex']))
+        worksheet.write(i, 3, label=str(information['PatientAge']))
+        worksheet.write(i, 4, label=str(information['KVP']))
+        worksheet.write(i, 5, label=str(information['PixelSpacing']))
+        worksheet.write(i, 6, label=str(information['SliceThickness']))
+        worksheet.write(i, 7, label=str(information['XRayTubeCurrent']))
+        worksheet.write(i, 8, label=str(information['Manufacturer']))
+    workbook.save(r'G:\Lobectomy\shengjing\RLL_info.xls')  # 保存
